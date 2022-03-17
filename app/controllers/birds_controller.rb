adca7fc04529ee87c9ff9e09f1ebb1a039e37872
Rebekah Zhou <rebekahzhou@gmail.com>
2022-03-17 10:22:34 -0700

    commit tree b7bc871a5e30b0e7add08db150776144dda9b06a

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,23 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create 
+    bird = Bird.create(bird_params)
+    if bird.valid?
+      render json: bird, status: :created
+    else 
+      render json: { errors: bird.error.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    bird = Bird.find(id: params[:id])
+    bird.delete
+  end
+
+  private
+  
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
